21512a01136ffb217f5f1d20a61d4c540c5d8206
Филипп Ковязин <filipp.a.kovyazin@gmail.com>
2025-05-12 14:55:57 +0500

    end_fun_mask_account_card

--- src/masks.py
+++ src/masks.py
@@ -1,12 +1,12 @@
 def get_mask_card_number(number_card: list) -> str:
     """Функцию маскировки номера банковской карты"""
-    number_card_mask = f"{number_card[:4]} {number_card[4:6]}** **** {number_card[12:]}"
 
-    return str(number_card_mask)
+    number_card_mask = str(f"{number_card[:4]} {number_card[4:6]}** **** {number_card[12:]}")
+    return number_card_mask
 
 
 def get_mask_account(number_account: list) -> str:
     """Функцию маскировки номера банковского счета"""
-    number_account_mask = f"**{number_account[-4:]}"
 
-    return str(number_account_mask)
+    number_account_mask = str(f"**{number_account[-4:]}")
+    return number_account_mask
--- src/widget.py
+++ src/widget.py
@@ -1,6 +1,23 @@
-def mask_account_card(str_mask: str) -> str:
-    """ Обрабатывает информацию о картах и о счетах """
+from src.masks import get_mask_account, get_mask_card_number
+
+
+def mask_account_card(str_number_card_or_account: str, number_account: list, number_card: list) -> str:
+    """Обрабатывает информацию о картах и о счетах"""
+
+    if "Счет" in str_number_card_or_account or "счет" in str_number_card_or_account:  # используем маскировку счета
+        number_account = list(str_number_card_or_account[-20:])
+        get_mask_account(number_account)
+        number_account_mask = get_mask_account(number_account)
+        text = str(f"{list(str_number_card_or_account[:-21])} {number_account_mask}")
+
+    else:  # используем маскировку карты
+        number_card = list(str_number_card_or_account[-16:])
+        get_mask_card_number(number_card)
+        number_card_mask = get_mask_card_number(number_card)
+        text = str(f"{list(str_number_card_or_account[:-17])} {number_card_mask}")
+
+    return text
 
 
 def get_date(str_date: str) -> str:
-    """ Принимает строку с датой и возвращает дату в формате "ДД.ММ.ГГГГ" """
+    """Принимает строку с датой и возвращает дату в формате "ДД.ММ.ГГГГ" """
