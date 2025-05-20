4204c0c87100a7dd7bb1e1ed97978d86d296bb60
Филипп Ковязин <filipp.a.kovyazin@gmail.com>
2025-05-20 17:23:54 +0500

    исправил название функции

--- src/processing.py
+++ src/processing.py
@@ -2,21 +2,21 @@ from typing import List, Dict, Any
 from widget import get_date
 
 
-def filter_by_state(list_of_dictionaries: List[Dict[str, Any]], state="EXECUTED") -> List[Dict[str, Any]]:
+def filter_by_state(list_of_transactions: List[Dict[str, Any]], state="EXECUTED") -> List[Dict[str, Any]]:
     """Функция принимает список словарей и возвращает новый список словарей в которых содержится значение state"""
 
-    list_dictionaries_state = []
+    list_of_transactions_state = []
 
-    for i in list_of_dictionaries:
+    for i in list_of_transactions:
         if i["state"] == state:
-            list_dictionaries_state.append(i)
+            list_of_transactions_state.append(i)
 
-    return list_dictionaries_state
+    return list_of_transactions_state
 
 
-def sort_by_date(list_of_dictionaries: List[Dict[str, Any]], fun_sorted: bool = False) -> List[Dict[str, Any]]:
+def sort_by_date(list_of_transactions: List[Dict[str, Any]], fun_sorted: bool = False) -> List[Dict[str, Any]]:
     """Функция принимает список словарей и сортирует их по дате. Параметр сортировки по умолчанию - убывание"""
 
-    sorted_list = sorted(list_of_dictionaries, key=lambda x: get_date(x["date"]), reverse=not fun_sorted)
+    sorted_list = sorted(list_of_transactions, key=lambda x: get_date(x["date"]), reverse=not fun_sorted)
 
     return sorted_list
