79507bf99b15a53ff2fdbf1de141d0e93cf88a34
Филипп Ковязин <filipp.a.kovyazin@gmail.com>
2025-06-22 17:53:20 +0500

    Module masks

--- src/masks.py
+++ src/masks.py
@@ -1,12 +1,40 @@
 def get_mask_card_number(number_card: list) -> str:
     """Функцию маскировки номера банковской карты"""
 
-    number_card_mask = str(f"{number_card[:4]} {number_card[4:6]}** **** {number_card[12:]}")
+    # Проверяем, что в списке есть хотя бы один элемент
+    if not number_card or len(number_card[0]) < 16:
+        return "Некорректный номер карты"
+
+    # Получаем строку номера карты из первого элемента списка
+    number_card = number_card[0]
+
+    # Удаляем все нецифровые символы
+    number_card = ''.join(filter(str.isdigit, number_card))
+
+    if len(number_card) != 16:
+        return "Некорректный номер карты"
+
+    # Маскировка номера карты
+    number_card_mask = f"{number_card[:4]} {number_card[4:6]}** **** {number_card[12:]}"
     return number_card_mask
 
 
 def get_mask_account(number_account: list) -> str:
     """Функцию маскировки номера банковского счета"""
 
+    # Проверяем, что в списке есть хотя бы один элемент
+    if not number_account or len(number_account[0]) < 16:
+        return "Некорректный номер счета"
+
+    # Получаем строку номера карты из первого элемента списка
+    number_account = number_account[0]
+
+    # Удаляем все нецифровые символы
+    number_account = ''.join(filter(str.isdigit, number_account))
+
+    if len(number_account) != 20:
+        return "Некорректный номер счета"
+
+    # Маскировка номера счета
     number_account_mask = str(f"**{number_account[-4:]}")
     return number_account_mask
---
+++ tests/test_masks.py
@@ -0,0 +1,17 @@
+from src.masks import get_mask_card_number
+from src.masks import get_mask_account
+
+def test_get_mask_card_number_many():
+    assert get_mask_card_number(['1234567890123456']) == '1234 56** **** 3456'
+    assert get_mask_card_number(['12345678901234567890']) == "Некорректный номер карты"
+    assert get_mask_card_number(['92848927']) == "Некорректный номер карты"
+    assert get_mask_card_number(['928489ds31']) == "Некорректный номер карты"
+    assert get_mask_card_number(['abfjffnjfsnojfno']) == "Некорректный номер карты"
+    assert get_mask_card_number(['']) == "Некорректный номер карты"
+
+def test_get_mask_account():
+    assert get_mask_account(['12345678901234567890']) == '**7890'
+    assert get_mask_account(['12345678901234567890123']) == "Некорректный номер счета"
+    assert get_mask_account(['242799']) == "Некорректный номер счета"
+    assert get_mask_account(['оjdsnljknls']) == "Некорректный номер счета"
+    assert get_mask_account(['']) == "Некорректный номер счета"
