5fe4f4e32f328ed63dba5f54d0dbb4b437e38e52
Филипп Ковязин <filipp.a.kovyazin@gmail.com>
2025-06-23 17:33:18 +0500

    дописал проверку функции sort_by_date

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,9 +1,9 @@
+import pytest
 from src.processing import filter_by_state, sort_by_date
 
 
 # Модуль processing
-@pytest.mark.parametrize(
-    "transactions, state, expected",
+@pytest.mark.parametrize("transactions, state, expected",
     [
         (
             [{"id": 1, "state": "EXECUTED"}, {"id": 2, "state": "PENDING"}, {"id": 3, "state": "EXECUTED"}],
@@ -31,3 +31,29 @@ from src.processing import filter_by_state, sort_by_date
 )
 def test_filter_by_state(transactions, state, expected):
     assert filter_by_state(transactions, state) == expected
+
+
+@pytest.mark.parametrize("transactions, date, expected",
+    (
+        [{"id": 1, "date": "/1999-01-20"}, {"id": 2, "date": "/1999-01-21"}, {"id": 3, "date": "/1999-01-22"}],
+        "True",
+        [{"id": 1, "date": "/1999-01-20"}, {"id": 2, "date": "/1999-01-21"}, {"id": 3, "date": "/1999-01-22"}],
+    ),
+    (
+         [{"id": 1, "date": "/1999-01-20"}, {"id": 2, "date": "/1999-01-21"}, {"id": 3, "date": "/1999-01-22"}],
+         "False",
+         [{"id": 3, "date": "/1999-01-22"}, {"id": 2, "date": "/1999-01-21"}, {"id": 1, "date": "/1999-01-20"}],
+    ),
+    (
+        [{"id": 1, "date": ""}, {"id": 2, "date": "/1999-01-21"}, {"id": 3, "date": "/1999-01-22"}],
+        "True",
+        [{"id": 2, "date": "/1999-01-21"}, {"id": 3, "date": "/1999-01-22"}],
+    ),
+    (
+        [{"id": 1, "date": ""}, {"id": 2, "date": "/1999-01-21"}, {"id": 3, "date": "/1999-01-22"}],
+        "False",
+        [{"id": 3, "date": "/1999-01-21"}, {"id": 2, "date": "/1999-01-22"}],
+    ),
+)
+def test_sort_by_date(transactions, fun_sorted, expected):
+    assert sort_by_date(transactions, fun_sorted) == expected
